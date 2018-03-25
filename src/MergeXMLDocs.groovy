0bba36ed8ea3826679abf1eeb2ed22e405685350
shail-burman <sburman@boomi.com>
2018-03-25 11:25:52 -0700

    Initial Project Commit

--- src/MergeXMLDocs.groovy
+++ src/MergeXMLDocs.groovy
@@ -1,18 +1,17 @@
 import groovy.io.FileType
 import groovy.util.slurpersupport.GPathResult
 import groovy.util.slurpersupport.NodeChild
-import groovy.xml.XmlUtil
 
 class MergeXMLDocs {
 
-   private NodeRepo
-   private xml_file_data = []
+    private NodeRepo
+    private xml_file_data = []
 
-   MergeXMLDocs() {
+    MergeXMLDocs() {
         NodeRepo = [:]
     }
 
-    public void addFileData(GPathResult xml_data ){
+    public void addFileData(GPathResult xml_data) {
         xml_file_data << xml_data
     }
 
@@ -23,45 +22,41 @@ class MergeXMLDocs {
         println NodeRepo
     }
 
-    private void parsefFileData(GPathResult Root){
-        Root.children().each {parseNode(it)}
+    private void parsefFileData(GPathResult Root) {
+        Root.children().each { parseNode(it) }
     }
 
-    private void parseNode(NodeChild Node){
-        if(Node.childNodes().size() > 1){
-            Node.children().each{parseNode(it)}
-        }
-        else{
-            if(Node.text() != '') {
+    private void parseNode(NodeChild Node) {
+        if (Node.childNodes().size() > 1) {
+            Node.children().each { parseNode(it) }
+        } else {
+            if (Node.text() != '') {
                 def NodeKey = getNodeHierarchy(Node)
                 def NodeData = Node.text()//XmlUtil.serialize(Node).replaceAll('\\<\\?(.)*\\?\\>', '')
-                if(this.NodeRepo.size() >0 ){
+                if (this.NodeRepo.size() > 0) {
                     if (!this.NodeRepo.containsKey(NodeKey)) {
                         this.NodeRepo.put(NodeKey, NodeData)
                     }
-                }
-                else{
+                } else {
                     this.NodeRepo.put(NodeKey, NodeData)
                 }
             }
         }
     }
 
-    private String getNodeHierarchy(NodeChild Node){
-        if(Node.parent() != Node){
+    private String getNodeHierarchy(NodeChild Node) {
+        if (Node.parent() != Node) {
             return getNodeHierarchy(Node.parent()) + '/' + Node.name()
-        }
-        else
+        } else
             return Node.name()
     }
-
 
 
     static void main(String[] args) {
         MergeXMLDocs Merger = new MergeXMLDocs()
         def FOLDER_PATH = '../Data/'
         def dir = new File(FOLDER_PATH)
-        dir.eachFileRecurse (FileType.FILES) { file ->
+        dir.eachFileRecurse(FileType.FILES) { file ->
             Merger.addFileData(new XmlSlurper().parse(file))
         }
         Merger.Process();
